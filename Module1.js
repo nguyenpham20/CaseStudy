d795d066cdfc632d18508c8688f0f47c2f95e8e6
nguyen <nguyenp021098@gmail.com>
2020-04-29 08:53:26 +0700

    BT

--- Module1.js
+++ Module1.js
@@ -1,30 +1,152 @@
-//B1: Tạo giao diện.
-//B1.1: Tao qua bong.
-let c = document.getElementById("myCanvas");
-let ctx = c.getContext("2d");
-let x = 20, y = 20;
-let dx = 11, dy =3;
-let radius = 10;
-function ball() {
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+let ballRadius = 10;
+
+let x = canvas.width/2;
+let y = canvas.height-30;
+
+let dx = 2;
+let dy = -2;
+
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width-paddleWidth)/2;
+
+let rightPressed = false;
+let leftPressed = false;
+
+//Khai bao hang gach
+let brickRowCount = 3;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+
+let bricks = [];
+
+let score = 0;
+
+for (let c=0; c<brickColumnCount; c++){
+    bricks[c] = [];
+    for (let r=0; r<brickRowCount; r++) {
+        bricks[c][r] = {x: 0, y: 0, status: 1};
+    }
+}
+
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
+function collisionDetection() {
+    for (let c=0; c<brickColumnCount; c++) {
+        for (let r=0; r<brickRowCount; r++) {
+            let b = bricks[c][r];
+            if (b.status == 1) {
+                if (x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight){
+                    dy = -dy;
+                    b.status = 0;
+                    score++;
+                    if(score == brickRowCount*brickColumnCount) {
+                        alert("YOU WIN, CONGRATULATIONS!");
+                        document.location.reload();
+                        clearInterval(interval); // Needed for Chrome to end game
+                    }
+                }
+            }
+        }
+    }
+}
+
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: " +score, 8, 20);
+}
+
+function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2*Math.PI);
-    ctx.fillStyle = "#d3d";
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2, false);
+    ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
-//B1.2: Tao Bong nhay
-function draw() {
-    ctx.clearRect(0, 0, c.width, c.height);
-    ball();
-    if (x-radius<0|| x+radius > c.width){
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawBricks() {
+    for (let c=0; c<brickColumnCount; c++) {
+        for (let r=0; r<brickRowCount; r++) {
+            if (bricks[c][r].status == 1){ let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+                let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+function draw(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    drawBricks();
+    drawPaddle();
+    drawScore();
+    collisionDetection();
+
+
+    if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-    if (y-radius<0|| y+radius>c.height){
+    if (y + dy < ballRadius) {
         dy = -dy;
-    }
-    x +=dx;
-    y +=dy;
-    requestAnimationFrame(draw);
-}
-draw();
+    }else if (y + dy > canvas.height-ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            if (y = y - paddleHeight){
+                dy = -dy;
+            }
 
+        }else{
+            alert("Game over!");
+            document.location.reload();
+            clearInterval(interval);
+        }
+    }
+
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX += 7;
+    }else if (leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+    x += dx;
+    y += dy;
+}
+
+let interval = setInterval(draw,10);
